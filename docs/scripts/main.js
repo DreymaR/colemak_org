e8cb8a89f52a3de5f26f2beedd236c9e4fe29554
abrickinthehallway <m.velichkov@outlook.com>
2021-11-03 15:02:38 +0200

    Added functionality for spoilers; Added FAQ section

---
+++ docs/scripts/main.js
@@ -0,0 +1,23 @@
+document.onload = function () {
+    hookSpoilersToggleOnClick([].slice.call(document.getElementsByClassName('spoiler')));
+    console.log(44)
+}();
+
+function hookSpoilersToggleOnClick(spoilersSelector) {
+    let spoilers = spoilersSelector;
+    spoilers.forEach(spoiler => spoiler.children[0].addEventListener('click', toggleSpoiler));
+    console.log(55)
+}
+
+function toggleSpoiler() {
+    let spoilerBody = this.parentNode.getElementsByClassName("spoilerBody")[0];
+    if (spoilerBody.classList.contains('active')) {
+        spoilerBody.classList.remove('active');
+        spoilerBody.parentNode.classList.remove('active');
+    }
+    else {
+        spoilerBody.classList.add('active');
+        spoilerBody.parentNode.classList.add('active');
+    }
+    console.log(66)
+}
